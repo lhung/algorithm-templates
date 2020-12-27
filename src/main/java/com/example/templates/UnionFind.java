a33e2c29c58c39fdac058943d0b827326f0fad4d
Lester Hung <lester.hung@hulu.com>
2020-12-27 14:34:37 -0800

    union find

---
+++ src/main/java/com/example/templates/UnionFind.java
@@ -0,0 +1,24 @@
+package com.example.templates;
+
+import java.util.Arrays;
+
+public class UnionFind {
+
+    private int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        Arrays.setAll(parent, i -> i);
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        parent[find(x)] = parent[find(y)];
+    }
+}
---
+++ src/test/java/com/example/templates/UnionFindTest.java
@@ -0,0 +1,16 @@
+package com.example.templates;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UnionFindTest {
+
+    @Test
+    public void test() {
+        UnionFind storage = new UnionFind(3);
+        storage.union(0,2);
+        storage.union(1,2);
+        assertEquals(storage.find(0), storage.find(1));
+    }
+}
